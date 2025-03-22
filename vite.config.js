cdeb041a1429c38b9b140f1e6297abd859e405c9
churrasstreams <joaomiguelsilvaaaa@gmail.com>
2025-03-22 18:11:10 +0000

    caguei teste 2

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/churrasstreams.github.io/', // substitui por o nome do teu repositório do GitHub
-  plugins: [react()],
-});
